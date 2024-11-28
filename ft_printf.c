4a65cddf13c8d3420a88b356259d669af189c8d0
mertilhans <160387403+mertilhans@users.noreply.github.com>
2024-11-28 07:33:36 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merilhan <merilhan@42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 07:30:44 by merilhan          #+#    #+#             */
+/*   Updated: 2024/11/28 07:31:07 by merilhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include <stdarg.h>
 
@@ -9,24 +21,24 @@ static int	ft_check(char type)
 	return (0);
 }
 
-static int  ft_format(va_list argv, char format)
+static int	ft_format(va_list argv, char format)
 {
-    if (format == 'c')
-        return(ft_print_char(va_arg(argv, int)));
-    else if ( format == 's')
-        return(ft_print_string(va_arg(argv,char *)));
-    else if (format == 'p')
-        return(ft_print_hex(va_arg(argv,unsigned long),format , 0));
-    else if (format == 'd' || format == 'i')
-        return(ft_print_integer(va_arg(argv, int), 0));
-    else if (format == 'u')
-        return(ft_print_integer(va_arg(argv, unsigned int), 0));
-    else if (format == 'x' || format == 'X')
-        return(ft_print_hex(va_arg(argv, unsigned int), format, 0));
-    else if (format == '%')
-        return (ft_print_char('%'));
-    else   
-        return (0);
+	if (format == 'c')
+		return (ft_print_char(va_arg(argv, int)));
+	else if (format == 's')
+		return (ft_print_string(va_arg(argv, char *)));
+	else if (format == 'p')
+		return (ft_print_hex(va_arg(argv, unsigned long), format, 0));
+	else if (format == 'd' || format == 'i')
+		return (ft_print_integer(va_arg(argv, int), 0));
+	else if (format == 'u')
+		return (ft_print_integer(va_arg(argv, unsigned int), 0));
+	else if (format == 'x' || format == 'X')
+		return (ft_print_hex(va_arg(argv, unsigned int), format, 0));
+	else if (format == '%')
+		return (ft_print_char('%'));
+	else
+		return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -55,51 +67,4 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(argv);
 	return (len);
-}
-int main()
-{
-
-char c = 'A';
-printf("Expected: %c\n", c);        
-ft_printf("Expected: %c\n", c);        
-
-
-int d = 12345;
-printf("Expected: %d\n", d);       
-ft_printf("Expected: %d\n", d);
-
-
-int i = -12345;
-printf("Expected: %i\n", i);              
-ft_printf("Expected: %i\n", i);         
-
-
-unsigned int u = 12345;
-printf("Expected: %u\n", u);              
-ft_printf("Expected: %u\n", u);       
-
-
-printf("Expected: %%\n");                 
-ft_printf("Expected: %%\n");               
-
-
-char *str = "Hello, World!";
-printf("Expected: %s\n", str);             
-ft_printf("Expected: %s\n", str);   
-
-
-unsigned int x = 255;
-printf("Expected: %x\n", x);             
-ft_printf("Expected: %x\n", x);      
-
-
-unsigned int X = 255;
-printf("Expected: %X\n", X);              
-ft_printf("Expected: %X\n", X);
-
-
-void *p = &str;
-printf("Expected: %p\n", p);              
-ft_printf("Expected: %p\n", p);  
-
 }
--- ft_utils.c
+++ ft_utils.c
@@ -1,72 +1,84 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merilhan <merilhan@42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 07:31:16 by merilhan          #+#    #+#             */
+/*   Updated: 2024/11/28 07:32:07 by merilhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
+#include <unistd.h>
 
-int ft_print_char(int c)
+int	ft_print_char(int c)
 {
-    return(write(1,&c,1));
+	return (write(1, &c, 1));
 }
 
-int ft_print_string(char *str)
+int	ft_print_string(char *str)
 {
-    int i;
-    i = 0;
-    if(!str)
-    {
-        if(write(1,"(nil)",5) == -1)
-            return (-1);
-        return (5);
-    }
-    while(str[i])
-        if (ft_print_char(str[i++]) == -1)
-            return (-1);
-    return (i);
-}
-int ft_print_hex(unsigned long nbr, char format, int len)
-{
-    char digit;
-    
-    if (nbr == 0 && len >= 0) {
-        len += ft_print_char('0');
-        return len;
-    }
-    
-    if (format == 'p') {
-        len += ft_print_string("0x"); 
-    }
+	int	i;
 
-    if (nbr > 0) {
-        if (nbr >= 16) {
-            len = ft_print_hex(nbr / 16, format, len); 
-        }
-        
-        if (format == 'x' || format == 'p')
-            digit = "0123456789abcdef"[nbr % 16];
-        else
-            digit = "0123456789ABCDEF"[nbr % 16];
-        
-        len += ft_print_char(digit); 
-    }
-
-    return len;
+	i = 0;
+	if (!str)
+	{
+		if (write(1, "(nil)", 5) == -1)
+			return (-1);
+		return (5);
+	}
+	while (str[i])
+		if (ft_print_char(str[i++]) == -1)
+			return (-1);
+	return (i);
 }
 
-int ft_print_integer(long nbr, int len)
+int	ft_print_hex(unsigned long nbr, char format, int len)
 {
-    if (nbr == 0 && len >= 0) {
-        len += ft_print_char('0'); 
-        return len;
-    }
+	char	digit;
 
-    if (nbr < 0) {
-        len += ft_print_char('-'); 
-        nbr = -nbr; 
-    }
+	if (nbr == 0 && len >= 0)
+	{
+		len += ft_print_char('0');
+		return (len);
+	}
+	if (format == 'p')
+	{
+		len += ft_print_string("0x");
+	}
+	if (nbr > 0)
+	{
+		if (nbr >= 16)
+		{
+			len = ft_print_hex(nbr / 16, format, len);
+		}
+		if (format == 'x' || format == 'p')
+			digit = "0123456789abcdef"[nbr % 16];
+		else
+			digit = "0123456789ABCDEF"[nbr % 16];
+		len += ft_print_char(digit);
+	}
+	return (len);
+}
 
-    if (nbr >= 10) {
-        len = ft_print_integer(nbr / 10, len); 
-    }
-
-    len += ft_print_char("0123456789"[nbr % 10]); 
-
-    return len;
+int	ft_print_integer(long nbr, int len)
+{
+	if (nbr == 0 && len >= 0)
+	{
+		len += ft_print_char('0');
+		return (len);
+	}
+	if (nbr < 0)
+	{
+		len += ft_print_char('-');
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+	{
+		len = ft_print_integer(nbr / 10, len);
+	}
+	len += ft_print_char("0123456789"[nbr % 10]);
+	return (len);
 }
